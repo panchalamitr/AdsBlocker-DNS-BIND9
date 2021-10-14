cb6ef6277c153b7671962f893707aaff73b7bc04
Amit Panchal <65744202+panchalamitr@users.noreply.github.com>
2021-10-14 11:15:09 +0800

    Update adsblocker.sh

--- adsblocker.sh
+++ adsblocker.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 echo "...Start..."
-sudo apt-get update
-sudo apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
 echo "...Installing Bind9..."
 # Install Bind9 (DNS Server)
-sudo apt-get install -y bind9
+apt-get install -y bind9
 
 echo "...Copying Configuration Files..."
-sudo cp named.conf /etc/bind/named.conf
-sudo cp adsblocker.py /etc/bind/adsblocker.py
+cp named.conf /etc/bind/named.conf
+cp adsblocker.py /etc/bind/adsblocker.py
 
 echo "...Fetch updated domain list..."
-sudo python3 adsblocker.py
+python3 adsblocker.py
 
 echo "...Restart Services..."
-sudo systemctl restart named
+systemctl restart named
 
-sudo systemctl restart bind9
+systemctl restart bind9
 
 echo "...Verify Data..."
-sudo named-checkconf
-sudo named-checkzone rpz /etc/bind/adsblocker.db
+named-checkconf
+named-checkzone rpz /etc/bind/adsblocker.db
 
 #Schedule Cron Job to fetch updated domain list
 echo "...Set Cronjob for future update..."
-sudo crontab -l > adblocker
+crontab -l > adblocker
 #cron job which will run at 00:00 on Sunday.
 echo "0 0 * * 0 sudo python3 /etc/bind/adsblocker.py >> /etc/bind/adsblocker.log" >> adblocker
 #install new cron file
-sudo crontab adblocker
+crontab adblocker
 
-sudo rm adblocker
+rm adblocker
 echo "...Finish..."
 
 
